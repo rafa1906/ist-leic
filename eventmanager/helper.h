2880ea4c2606f9f49c72d47480b8cda350b54f22
Rafael Gonçalves <rafaelgonrg196@gmail.com>
2019-06-26 00:24:03 +0100

    Translated helper.h

--- eventmanager/helper.h
+++ eventmanager/helper.h
@@ -1,45 +1,67 @@
 /*
-* Ficheiro: helper.h
-* Autor: Rafael Goncalves 92544
-* Descricao: Header do organizador de eventos; contem constantes e estruturas
+* File: helper.h
+* Author: Rafael Goncalves
+* Description: Header for the event manager
 */
 
 
-/* CONSTANTES */
-#define STRLEN 64               /* Tamanho maximo de string */
-#define MAXPART 3               /* Numero maximo de participantes */
-#define MAXPEOPLE 4             /* Numero maximo de pessoas presentes */
-#define MAXEVENTS 1000          /* Numero maximo de eventos */
-#define MINUTES 60              /* Numero de minutos numa hora */
-#define SHIFTTWO 100            /* Faz um shift left de 2 */
-#define SHIFTFOUR 10000         /* Faz um shift left de 4 */
-#define SHIFTSIX 1000000        /* Faz um shift left de 6 */
+#define STRLEN 64               /* Max string length */
+#define MAXPART 3               /* Max number of participants */
+#define MAXPEOPLE 4             /* Max number of people in an event */
+#define MAXEVENTS 1000          /* Max number of scheduled events */
+#define MINUTES 60              /* Minutes in an hour */
+#define SHIFTTWO 100            /* Left shift of 2 */
+#define SHIFTFOUR 10000         /* Left shift of 4 */
+#define SHIFTSIX 1000000        /* Left shift of 6 */
 
 
-/* ESTRUTURAS */
-/* Tipo booleano */
-typedef enum { false, true } bool;
+typedef enum { false, true } bool;      /* Boolean */
 
-/* Tipo data (dia/mes/ano) */
-typedef struct date {
+typedef struct date {       /* Holds a date */
     int day;
     int month;
     int year;
 } Date;
 
-/* Tipo tempo (hora/minuto) */
-typedef struct time {
+typedef struct time {       /* Holds a time */
     int hour;
     int min;
 } Time;
 
-/* Tipo evento */
-typedef struct event {
-    char description[STRLEN];               /* Guarda a descricao do evento */
-    Date date;                              /* Guarda a data do evento */
-    Time start;                             /* Guarda a hora de comeco do evento */
-    int duration;                           /* Guarda a duracao do evento */
-    int room;                               /* Guarda a sala do evento */
-    char organiser[STRLEN];                 /* Guarda o organizador do evento */
-    char participants[MAXPART][STRLEN];     /* Guarda os participantes do evento */
+typedef struct event {      /* Holds an event */
+    char description[STRLEN];     /* Event description */
+    Date date;     /* Event date */
+    Time start;     /* Event start time */
+    int duration;     /* Event duration */
+    int room;     /* Event room */
+    char organiser[STRLEN];     /* Event organiser */
+    char participants[MAXPART][STRLEN];     /* Event participants (up to 3) */
 } Event;
+
+
+void add_event();
+void list_all();
+void list_room();
+void remove_event();
+void change_start();
+void change_duration();
+void change_room();
+void add_part();
+void remove_part();
+bool push_event(Event e, int control);
+Event pop_event(int pos);
+void change_event(char key[], char new_attribute[], int control);
+bool catch_errors(Event e, int control);
+void print_results(Event e);
+char * read_input();
+Event init_event();
+bool is_valid_field(char field[]);
+bool earlier(Event e1, Event e2);
+bool same_time(Event e1, Event e2);
+bool incompatible_times(Event e1, Event e2);
+Date str_to_date(char * str);
+int date_to_int(Date date);
+int date_to_fint(Date date);
+Time str_to_time(char * str);
+int time_to_int(Time time);
+Time get_finish_time(Event e);
